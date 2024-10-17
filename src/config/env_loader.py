7660cda35cc6794d501ecaef03dbeeb95e903790
little-euphie <anisphia@VanadisHQ>
2024-10-17 16:39:09 +0700

    initialized logger, environment & dependencies

---
+++ src/config/env_loader.py
@@ -0,0 +1,11 @@
+from dotenv import load_dotenv
+import os
+
+# Load environment variables from the .env file
+load_dotenv()
+
+POSTGRE_PORT=int(os.getenv('POSTGRE_PORT'))
+POSTGRE_HOST=os.getenv('POSTGRE_HOST')
+POSTGRE_DATABASE=os.getenv('POSTGRE_DATABASE')
+POSTGRE_USERNAME=os.getenv('POSTGRE_USERNAME')
+POSTGRE_PASSWORD=os.getenv('POSTGRE_PASSWORD')
